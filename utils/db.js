e0b04f0308664117862a62b75c21b963f033a661
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-06-25 15:05:17 +0100

    [FEAT] MongoDB Utils
    Created a DBClient class which creates a client to mongodb, chls of collections

--- utils/db.js
+++ utils/db.js
@@ -1,10 +1,42 @@
 import { MongoClient } from 'mongodb';
 
-class DBClient{
-  constructor () {
-    this.client = new MongoClient();
-    this.host = process.env.DB_HOST || "localhost";
-    this.port = process.env.DB_PORT || 27017;
-    this.database = process.env.DB_DATABASE || 'files_manager';
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const database = process.env.DB_DATABASE || 'files_manager';
+const connUrl = `mongodb://${host}:${port}`;
+class DBClient {
+  constructor() {
+    // console.log(connUrl);
+    this.client = new MongoClient(connUrl, { useUnifiedTopology: true });
+    // { useUnifiedTopology: true } new server discover & monitoring engine
+    this.client.connect()
+      .then(() => {
+        this.db = this.client.db(`${database}`);
+      }).catch((err) => {
+        console.log(err);
+      });
+  }
+
+  isAlive() {
+    // switches from connecting to connected once a connection
+    // has been established
+    const status = this.client.topology.s.state;
+    if (status === 'connected') { return true; } return false;
+  }
+
+  async nbUsers() {
+    const users = this.db.collection('users');
+    const num = await users.countDocuments();
+    return num;
+  }
+
+  async nbFiles() {
+    const files = this.db.collection('files');
+    const num = await files.countDocuments();
+    return num;
   }
 }
+
+const dbClient = new DBClient();
+
+module.exports = dbClient;
