f15bd10e96aacee53708b205fe1a6dd2c2193a27
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-06-27 21:17:12 +0100

    [CHORE] Module Exports

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import router from './routes/index';
+const express = require('express');
+const router = require('./routes/index');
 
 const PORT = process.env.PORT || 5000;
 const app = express(); // app created
@@ -9,4 +9,6 @@ app.use(express.json());
 // register all imported routes to the app starting at '/' i.e root path
 app.use('/', router);
 
-export default app.listen(PORT, () => { });
+app.listen(PORT, () => { });
+
+module.exports = app;
